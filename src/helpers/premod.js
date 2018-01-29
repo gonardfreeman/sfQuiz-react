a2751a79a816a183dfc9c055a77b4ae72fe2b719
Dmytro Bondarenko <dbondarenko@dynamo-kiev.local>
2018-01-29 12:31:14 +0200

    deleted 18 quesxtion from random

--- src/helpers/premod.js
+++ src/helpers/premod.js
@@ -1,6 +1,6 @@
 export function getRandomInt(max, pull) {
   let rand = Math.floor(Math.random() * Math.floor(max));
-  if (!~pull.indexOf(rand)) {
+  if (!~pull.indexOf(rand) && rand !== 18) {
     return rand;
   }
   return Math.floor(Math.random() * Math.floor(max));
